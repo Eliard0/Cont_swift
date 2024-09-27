5ac63e6b219bbeed7eb3c5aa7d6650964ac41198
Eliardo <eliardosilva81@gmail.com>
2024-09-27 14:41:57 -0300

    Initial Commit

---
+++ Contador/ContadorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContadorApp.swift
+//  Contador
+//
+//  Created by Eliardo Venancio on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ContadorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
